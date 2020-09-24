f1bed3d95dc336b62d9403d94bc139093c978e6e
Claudiu Giurumescu <claudiu.giurumescu@gmail.com>
2020-09-23 22:37:29 -0700

    add NexysVideo project 004 Microblaze Linux

---
+++ BOARDS/NEXYSVIDEO/004_microblaze_linux/README.md
@@ -0,0 +1,15 @@
+# Design details
+This is an initial attempt to run Linux on NexysVideo using MicroBlaze. While
+developing the project the following items were revealed:
+- QSPI support seems broken
+- no SD card support, needs driver and AXI2SDCARD IP.
+- Ethernet requires paid 1G/2.5 Ethernet IP from Xilinx.
+- Uart can be used at 9600 BAUD.
+
+This leaves us with only the option of booting using JTAG and using 
+initramfs for rootfs.
+
+Another interesting caveat about this project (which is largely)
+due to the fact that I wanted to keep the bd design in a known
+location not stashed under Vivado's project was documented
+[here](https://forums.xilinx.com/t5/Xilinx-IP-Catalog/IP-Flow-19-3460-Validation-failed-on-parameter-XML-INPUT-FILE/m-p/1154478#M8199)
---
+++ BOARDS/NEXYSVIDEO/004_microblaze_linux/bd/design_top_bd/hdl/design_top_bd_wrapper.v
@@ -0,0 +1,88 @@
+//Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2019.1_AR72614 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
+//Date        : Tue Sep 22 23:24:10 2020
+//Host        : TOSHIBAP755 running 64-bit Linux Mint 19.2 Tina
+//Command     : generate_target design_top_bd_wrapper.bd
+//Design      : design_top_bd_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_top_bd_wrapper
+   (DDR3_addr,
+    DDR3_ba,
+    DDR3_cas_n,
+    DDR3_ck_n,
+    DDR3_ck_p,
+    DDR3_cke,
+    DDR3_dm,
+    DDR3_dq,
+    DDR3_dqs_n,
+    DDR3_dqs_p,
+    DDR3_odt,
+    DDR3_ras_n,
+    DDR3_reset_n,
+    DDR3_we_n,
+    cpu_resetn,
+    sysclock,
+    uart_rx_out,
+    uart_tx_in);
+  output [14:0]DDR3_addr;
+  output [2:0]DDR3_ba;
+  output DDR3_cas_n;
+  output [0:0]DDR3_ck_n;
+  output [0:0]DDR3_ck_p;
+  output [0:0]DDR3_cke;
+  output [1:0]DDR3_dm;
+  inout [15:0]DDR3_dq;
+  inout [1:0]DDR3_dqs_n;
+  inout [1:0]DDR3_dqs_p;
+  output [0:0]DDR3_odt;
+  output DDR3_ras_n;
+  output DDR3_reset_n;
+  output DDR3_we_n;
+  input cpu_resetn;
+  input sysclock;
+  output uart_rx_out;
+  input uart_tx_in;
+
+  wire [14:0]DDR3_addr;
+  wire [2:0]DDR3_ba;
+  wire DDR3_cas_n;
+  wire [0:0]DDR3_ck_n;
+  wire [0:0]DDR3_ck_p;
+  wire [0:0]DDR3_cke;
+  wire [1:0]DDR3_dm;
+  wire [15:0]DDR3_dq;
+  wire [1:0]DDR3_dqs_n;
+  wire [1:0]DDR3_dqs_p;
+  wire [0:0]DDR3_odt;
+  wire DDR3_ras_n;
+  wire DDR3_reset_n;
+  wire DDR3_we_n;
+  wire cpu_resetn;
+  wire sysclock;
+  wire uart_rx_out;
+  wire uart_tx_in;
+
+  design_top_bd design_top_bd_i
+       (.DDR3_addr(DDR3_addr),
+        .DDR3_ba(DDR3_ba),
+        .DDR3_cas_n(DDR3_cas_n),
+        .DDR3_ck_n(DDR3_ck_n),
+        .DDR3_ck_p(DDR3_ck_p),
+        .DDR3_cke(DDR3_cke),
+        .DDR3_dm(DDR3_dm),
+        .DDR3_dq(DDR3_dq),
+        .DDR3_dqs_n(DDR3_dqs_n),
+        .DDR3_dqs_p(DDR3_dqs_p),
+        .DDR3_odt(DDR3_odt),
+        .DDR3_ras_n(DDR3_ras_n),
+        .DDR3_reset_n(DDR3_reset_n),
+        .DDR3_we_n(DDR3_we_n),
+        .cpu_resetn(cpu_resetn),
+        .sysclock(sysclock),
+        .uart_rx_out(uart_rx_out),
+        .uart_tx_in(uart_tx_in));
+endmodule
